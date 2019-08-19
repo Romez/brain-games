4c9c3062506b2bd5c965503ce760b3725cde858a
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-08-19 19:58:12 +0300

    refactor game engine cycle

--- src/index.js
+++ src/index.js
@@ -13,24 +13,22 @@ const startGame = (title, play) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(greetUser(userName));
 
-  for (let i = 0; i <= maxTries; i += 1) {
-    if (i >= maxTries) {
-      console.log(green(`Congratulations ${userName}!`));
-      break;
-    }
-
+  for (let i = 0; i < maxTries; i += 1) {
     const { question, rightAnswer } = play();
     console.log(`Question: ${question}`);
 
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== rightAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-      break;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
 
     console.log(green('Correct!'));
   }
+
+  console.log(green(`Congratulations ${userName}!`));
 };
 
 export default startGame;
