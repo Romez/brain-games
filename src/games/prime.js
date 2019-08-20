be272c38fe75e7fb8adedc2f3b13219442d9549f
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-08-20 09:44:06 +0300

    fix prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,15 +1,20 @@
 import random from 'lodash/random';
 import gameEngine from '..';
 
-const maxRamdomNumber = 100;
+const minRandomNumber = -10;
+const maxRamdomNumber = 10;
 
 const isPrime = (num) => {
-  const square = x => x * x;
+  const firstPrimeNumber = 2;
+
+  if (num < firstPrimeNumber) {
+    return false;
+  }
 
   const isDevider = (testDevider, x) => x % testDevider === 0;
 
   const findDevider = (x, testDevider) => {
-    if (square(testDevider) > x) {
+    if (testDevider ** 2 > x) {
       return x;
     }
 
@@ -20,15 +25,13 @@ const isPrime = (num) => {
     return findDevider(x, testDevider + 1);
   };
 
-  const firstTestDevider = 2;
-
-  return num === findDevider(num, firstTestDevider);
+  return num === findDevider(num, firstPrimeNumber);
 };
 
 const title = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const play = () => {
-  const question = random(maxRamdomNumber);
+  const question = random(minRandomNumber, maxRamdomNumber);
   const rightAnswer = isPrime(question) ? 'yes' : 'no';
 
   return { question, rightAnswer };
